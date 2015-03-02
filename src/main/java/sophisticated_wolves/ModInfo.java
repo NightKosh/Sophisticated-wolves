d66b4c0ada6bfcb40bfc21dd4112f1def78d220d
NightKosh <nightmare@railfox.ru>
2015-03-02 04:00:14 +0300

    Update version

--- src/main/java/sophisticated_wolves/ModInfo.java
+++ src/main/java/sophisticated_wolves/ModInfo.java
@@ -14,5 +14,5 @@ public class ModInfo {
 
     public static final String ID = "SophisticatedWolves";
     public static final String NAME = "SophisticatedWolves";
-    public static final String VERSION = "3.4.1";
+    public static final String VERSION = "3.5.0";
 }
