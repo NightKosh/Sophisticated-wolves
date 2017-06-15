af712f5d6bd814cc079012d878063c3dca165865
NightKosh <nightkosh@gmail.com>
2017-06-15 20:42:29 +0300

    Fix

--- src/main/java/sophisticated_wolves/SWEntity.java
+++ src/main/java/sophisticated_wolves/SWEntity.java
@@ -10,7 +10,7 @@ import sophisticated_wolves.entity.EntitySophisticatedWolf;
 import java.util.Set;
 
 /**
- * GraveStone mod
+ * Sophisticated Wolves
  *
  * @author NightKosh
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
