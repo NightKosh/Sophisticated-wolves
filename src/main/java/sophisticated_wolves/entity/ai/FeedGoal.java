b9b7c4e22ffbc7ad24769440f61d00190c2f9c26
NightKosh <nightkosh@gmail.com>
2022-11-23 05:39:57 +0300

    Cancel feed goals after some time + check is distance can be passed

--- src/main/java/sophisticated_wolves/entity/ai/FeedGoal.java
+++ src/main/java/sophisticated_wolves/entity/ai/FeedGoal.java
@@ -1,9 +1,9 @@
 package sophisticated_wolves.entity.ai;
 
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import sophisticated_wolves.util.FoodUtils;
@@ -21,11 +21,14 @@ public class FeedGoal extends Goal {
     private final Level level;
     private final SophisticatedWolf pet;
 
+    private LivingEntity owner;
     protected ItemEntity foodEntity;
+    private int timeToStopGoal = 0;
 
-    public FeedGoal(SophisticatedWolf pet) {
-        this.pet = pet;
-        this.level = pet.getLevel();
+    public FeedGoal(SophisticatedWolf wolf) {
+        this.pet = wolf;
+        this.level = wolf.getLevel();
+        this.owner = wolf.getOwner();
     }
 
     /**
@@ -34,17 +37,20 @@ public class FeedGoal extends Goal {
     @Override
     public boolean canUse() {
         if (this.pet.isTame() &&
-                !this.pet.isOrderedToSit() ||
-                this.pet.getHealth() >= SWConfiguration.WOLVES_HEALTH_TAMED.get()) {
-            return false;
-        } else {
+                !this.pet.isOrderedToSit() &&
+                this.pet.getHealth() < SWConfiguration.WOLVES_HEALTH_TAMED.get()) {
+            if (this.owner == null) {
+                this.owner = this.pet.getOwner();
+            }
             var foodList = this.level.getEntitiesOfClass(ItemEntity.class,
                     new AABB(this.pet.getX() - 30, this.pet.getY() - 30, this.pet.getZ() - 30,
                             this.pet.getX() + 30, this.pet.getY() + 30, this.pet.getZ() + 30));
             for (ItemEntity foodEntity : foodList) {
                 var stack = foodEntity.getItem();
                 if ((FoodUtils.isFoodItem(stack) || FoodUtils.isBone(stack)) &&
-                        FoodUtils.isWolfFood(pet, stack)) {
+                        FoodUtils.isWolfFood(pet, stack) &&
+                        //item should be closer than teleportation range
+                        this.owner != null && this.owner.distanceToSqr(foodEntity) < SophisticatedWolf.DISTANCE_TO_TELEPORT_TO_OWNER_SQR) {
                     this.foodEntity = foodEntity;
                     return true;
                 }
@@ -58,8 +64,11 @@ public class FeedGoal extends Goal {
      */
     @Override
     public boolean canContinueToUse() {
-        return this.pet.getHealth() < SWConfiguration.WOLVES_HEALTH_TAMED.get() &&
-                this.foodEntity != null && this.foodEntity.isAlive();
+        return this.timeToStopGoal <= 100 &&
+                this.pet.getHealth() < SWConfiguration.WOLVES_HEALTH_TAMED.get() &&
+                this.foodEntity != null && this.foodEntity.isAlive() &&
+                //item should be closer than teleportation range
+                this.owner.distanceToSqr(foodEntity) < SophisticatedWolf.DISTANCE_TO_TELEPORT_TO_OWNER_SQR;
     }
 
     /**
@@ -68,6 +77,7 @@ public class FeedGoal extends Goal {
     @Override
     public void tick() {
         if (this.foodEntity != null) {
+            timeToStopGoal++;
             this.pet.getNavigation().moveTo(this.foodEntity, 1);
             if (this.pet.distanceTo(this.foodEntity) <= 1) {
                 this.pet.getLookControl().setLookAt(
@@ -91,6 +101,7 @@ public class FeedGoal extends Goal {
     @Override
     public void stop() {
         this.foodEntity = null;
+        this.timeToStopGoal = 0;
         this.pet.getNavigation().stop();
     }
 
