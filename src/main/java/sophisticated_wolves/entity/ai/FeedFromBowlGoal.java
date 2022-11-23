a5f7714fb77a3a7fcd650a5b70f2f82c63b3f1f6
NightKosh <nightkosh@gmail.com>
2022-11-23 05:39:57 +0300

    Cancel feed goals after some time + check is distance can be passed

--- src/main/java/sophisticated_wolves/entity/ai/FeedFromBowlGoal.java
+++ src/main/java/sophisticated_wolves/entity/ai/FeedFromBowlGoal.java
@@ -1,6 +1,7 @@
 package sophisticated_wolves.entity.ai;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.player.Player;
@@ -8,6 +9,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import sophisticated_wolves.core.SWConfiguration;
 import sophisticated_wolves.block_entity.BlockEntityDogBowl;
+import sophisticated_wolves.entity.SophisticatedWolf;
 
 import java.util.Map;
 
@@ -22,20 +24,24 @@ public class FeedFromBowlGoal extends Goal {
     private final TamableAnimal pet;
     private final Level level;
 
+    private LivingEntity owner;
+    private int timeToStopGoal = 0;
     private BlockEntityDogBowl dogBowl;
 
     public FeedFromBowlGoal(TamableAnimal animal) {
         this.pet = animal;
         this.level = animal.getLevel();
+        this.owner = animal.getOwner();
     }
 
     @Override
     public boolean canUse() {
-        if (!this.pet.isTame() ||
-                this.pet.isOrderedToSit() ||
-                this.pet.getHealth() >= SWConfiguration.WOLVES_HEALTH_TAMED.get()) {
-            return false;
-        } else {
+        if (this.pet.isTame() &&
+                !this.pet.isOrderedToSit() &&
+                this.pet.getHealth() < SWConfiguration.WOLVES_HEALTH_TAMED.get()) {
+            if (this.owner == null) {
+                this.owner = this.pet.getOwner();
+            }
             var x = this.pet.getX();
             var y = this.pet.getY();
             var z = this.pet.getZ();
@@ -50,6 +56,7 @@ public class FeedFromBowlGoal extends Goal {
                     getBowlTe(new BlockPos(x - 16, y, z + 16)) ||
                     getBowlTe(new BlockPos(x - 16, y, z - 16));
         }
+        return false;
     }
 
     private boolean getBowlTe(BlockPos pos) {
@@ -59,7 +66,10 @@ public class FeedFromBowlGoal extends Goal {
                 var x = beEntry.getKey().getX();
                 var y = beEntry.getKey().getY();
                 var z = beEntry.getKey().getZ();
-                if (this.pet.distanceToSqr(x, y, z) < 50 && bowl.getFoodAmount() > 0) {
+                if (bowl.getFoodAmount() > 0 &&
+                        this.pet.distanceToSqr(x, y, z) < 50 &&
+                        //item should be closer than teleportation range
+                        this.owner != null && this.owner.distanceToSqr(x, y, z) < SophisticatedWolf.DISTANCE_TO_TELEPORT_TO_OWNER_SQR) {
                     this.dogBowl = bowl;
                     return true;
                 }
@@ -70,13 +80,20 @@ public class FeedFromBowlGoal extends Goal {
 
     @Override
     public boolean canContinueToUse() {
-        return this.pet.getHealth() < SWConfiguration.WOLVES_HEALTH_TAMED.get() &&
-                this.dogBowl != null && this.dogBowl.getFoodAmount() > 0;
+        return this.timeToStopGoal <= 100 &&
+                this.pet.getHealth() < SWConfiguration.WOLVES_HEALTH_TAMED.get() &&
+                this.dogBowl != null && !this.dogBowl.isRemoved() && this.dogBowl.getFoodAmount() > 0 &&
+                //item should be closer than teleportation range
+                this.owner.distanceToSqr(
+                        this.dogBowl.getBlockPos().getX(),
+                        this.dogBowl.getBlockPos().getY(),
+                        this.dogBowl.getBlockPos().getZ()) < SophisticatedWolf.DISTANCE_TO_TELEPORT_TO_OWNER_SQR;
     }
 
     @Override
     public void tick() {
         if (this.dogBowl != null) {
+            timeToStopGoal++;
             this.pet.getNavigation().moveTo(
                     this.pet.getNavigation().createPath(this.dogBowl.getBlockPos(), 0),
                     1);
@@ -100,6 +117,7 @@ public class FeedFromBowlGoal extends Goal {
     @Override
     public void stop() {
         this.dogBowl = null;
+        this.timeToStopGoal = 0;
         this.pet.getNavigation().stop();
     }
 
