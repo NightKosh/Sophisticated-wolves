75629055933cfca28fb07fad580b54ef5d01f57b
NightKosh <nightkosh@gmail.com>
2022-11-23 17:06:10 +0300

    Rework feed goals

---
+++ src/main/java/sophisticated_wolves/entity/ai/AFeedGoal.java
@@ -0,0 +1,107 @@
+package sophisticated_wolves.entity.ai;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.level.Level;
+import sophisticated_wolves.core.SWConfiguration;
+import sophisticated_wolves.entity.SophisticatedWolf;
+
+/**
+ * Sophisticated Wolves
+ *
+ * @author NightKosh
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
+public abstract class AFeedGoal<T extends Object> extends Goal {
+
+    protected final Level level;
+    protected final SophisticatedWolf pet;
+
+    protected LivingEntity owner;
+    protected T feedObject;
+    private int timeToStopGoal = 0;
+
+    public AFeedGoal(SophisticatedWolf wolf) {
+        this.pet = wolf;
+        this.level = wolf.getLevel();
+        this.owner = wolf.getOwner();
+    }
+
+    /**
+     * Returns whether the Goal should begin execution.
+     */
+    @Override
+    public boolean canUse() {
+        if (this.pet.isTame() &&
+                !this.pet.isOrderedToSit() &&
+                this.pet.getHealth() < SWConfiguration.WOLVES_HEALTH_TAMED.get()) {
+            if (this.owner == null) {
+                this.owner = this.pet.getOwner();
+            }
+            return this.findFeedObject();
+        }
+        return false;
+    }
+
+    protected abstract boolean findFeedObject();
+
+    /**
+     * Returns whether an in-progress Goal should continue executing
+     */
+    @Override
+    public boolean canContinueToUse() {
+        return this.timeToStopGoal <= 100 &&
+                this.pet.getHealth() < SWConfiguration.WOLVES_HEALTH_TAMED.get() &&
+                this.feedObject != null && this.ifFeedObjectAlive() &&
+                //item should be closer than teleportation range
+                this.getDistanceSqrToFeedObject(this.owner) < SophisticatedWolf.DISTANCE_TO_TELEPORT_TO_OWNER_SQR;
+    }
+
+    protected abstract boolean ifFeedObjectAlive();
+
+    protected double getDistanceSqrToFeedObject(LivingEntity entity) {
+        return entity.distanceToSqr(
+                this.getFeedObjectPosX(),
+                this.getFeedObjectPosY(),
+                this.getFeedObjectPosZ());
+    }
+
+    protected abstract double getFeedObjectPosX();
+
+    protected abstract double getFeedObjectPosY();
+
+    protected abstract double getFeedObjectPosZ();
+
+    /**
+     * Updates Goal
+     */
+    @Override
+    public void tick() {
+        if (this.feedObject != null) {
+            timeToStopGoal++;
+            this.moveTo();
+            if (this.getDistanceSqrToFeedObject(this.pet) <= 1.3F) {
+                this.pet.getLookControl().setLookAt(
+                        this.getFeedObjectPosX(), this.getFeedObjectPosY(), this.getFeedObjectPosZ(),
+                        0.25F, 0.25F);
+
+                this.feed();
+            }
+        }
+    }
+
+    protected abstract void moveTo();
+
+    protected abstract void feed();
+
+    /**
+     * Resets Goal
+     */
+    @Override
+    public void stop() {
+        this.feedObject = null;
+        this.timeToStopGoal = 0;
+        this.pet.getNavigation().stop();
+    }
+
+}
--- src/main/java/sophisticated_wolves/entity/ai/FeedFromBowlGoal.java
+++ src/main/java/sophisticated_wolves/entity/ai/FeedFromBowlGoal.java
@@ -1,16 +1,12 @@
 package sophisticated_wolves.entity.ai;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.TamableAnimal;
-import net.minecraft.world.entity.ai.goal.Goal;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import sophisticated_wolves.core.SWConfiguration;
 import sophisticated_wolves.block_entity.BlockEntityDogBowl;
+import sophisticated_wolves.core.SWConfiguration;
 import sophisticated_wolves.entity.SophisticatedWolf;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -19,58 +15,91 @@ import java.util.Map;
  * @author NightKosh
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
-public class FeedFromBowlGoal extends Goal {
+public class FeedFromBowlGoal extends AFeedGoal<BlockEntityDogBowl> {
 
-    private final TamableAnimal pet;
-    private final Level level;
+    //TODO replace by R-tree ?
+    private static final Map<BlockPos, BlockEntity> BOWLS = new HashMap<>();
 
-    private LivingEntity owner;
-    private int timeToStopGoal = 0;
-    private BlockEntityDogBowl dogBowl;
-
-    public FeedFromBowlGoal(TamableAnimal animal) {
-        this.pet = animal;
-        this.level = animal.getLevel();
-        this.owner = animal.getOwner();
+    public FeedFromBowlGoal(SophisticatedWolf wolf) {
+        super(wolf);
     }
 
     @Override
-    public boolean canUse() {
-        if (this.pet.isTame() &&
-                !this.pet.isOrderedToSit() &&
-                this.pet.getHealth() < SWConfiguration.WOLVES_HEALTH_TAMED.get()) {
-            if (this.owner == null) {
-                this.owner = this.pet.getOwner();
-            }
-            var x = this.pet.getX();
-            var y = this.pet.getY();
-            var z = this.pet.getZ();
-            //TODO store bowls as list and look in this list first
-            return getBowlTe(this.pet.blockPosition()) ||
-                    getBowlTe(new BlockPos(x + 16, y, z)) ||
-                    getBowlTe(new BlockPos(x - 16, y, z)) ||
-                    getBowlTe(new BlockPos(x, y, z + 16)) ||
-                    getBowlTe(new BlockPos(x, y, z + 16)) ||
-                    getBowlTe(new BlockPos(x + 16, y, z + 16)) ||
-                    getBowlTe(new BlockPos(x + 16, y, z - 16)) ||
-                    getBowlTe(new BlockPos(x - 16, y, z + 16)) ||
-                    getBowlTe(new BlockPos(x - 16, y, z - 16));
-        }
-        return false;
+    protected boolean findFeedObject() {
+        return tryToGetBowl(BOWLS, false) || tryToGetBowlForNearestChunks(this.pet.blockPosition());
     }
 
-    private boolean getBowlTe(BlockPos pos) {
-        Map<BlockPos, BlockEntity> beMap = this.level.getChunkAt(pos).getBlockEntities();
-        for (Map.Entry<BlockPos, BlockEntity> beEntry : beMap.entrySet()) {
+    @Override
+    protected boolean ifFeedObjectAlive() {
+        return !this.feedObject.isRemoved() && this.feedObject.getFoodAmount() > 0;
+    }
+
+    @Override
+    protected double getFeedObjectPosX() {
+        return this.feedObject.getBlockPos().getX();
+    }
+
+    @Override
+    protected double getFeedObjectPosY() {
+        return this.feedObject.getBlockPos().getY();
+    }
+
+    @Override
+    protected double getFeedObjectPosZ() {
+        return this.feedObject.getBlockPos().getZ();
+    }
+
+    @Override
+    protected void moveTo() {
+        this.pet.getNavigation().moveTo(
+                this.pet.getNavigation().createPath(this.feedObject.getBlockPos(), 0),
+                1);
+    }
+
+    @Override
+    protected void feed() {
+        this.pet.heal(1);
+
+        this.feedObject.addFood(-1);
+        if (this.feedObject.getFoodAmount() <= 0 ||
+                this.pet.getHealth() >= SWConfiguration.WOLVES_HEALTH_TAMED.get()) {
+            this.stop();
+        }
+    }
+
+    private boolean tryToGetBowlForNearestChunks(BlockPos pos) {
+        var x = pos.getX();
+        var y = pos.getY();
+        var z = pos.getZ();
+        return tryToGetBowl(pos) ||
+                tryToGetBowl(new BlockPos(x + 16, y, z)) ||
+                tryToGetBowl(new BlockPos(x - 16, y, z)) ||
+                tryToGetBowl(new BlockPos(x, y, z + 16)) ||
+                tryToGetBowl(new BlockPos(x, y, z + 16)) ||
+                tryToGetBowl(new BlockPos(x + 16, y, z + 16)) ||
+                tryToGetBowl(new BlockPos(x + 16, y, z - 16)) ||
+                tryToGetBowl(new BlockPos(x - 16, y, z + 16)) ||
+                tryToGetBowl(new BlockPos(x - 16, y, z - 16));
+    }
+
+    private boolean tryToGetBowl(BlockPos pos) {
+        return tryToGetBowl(this.level.getChunkAt(pos).getBlockEntities(), true);
+    }
+
+    private boolean tryToGetBowl(Map<BlockPos, BlockEntity> beMap, boolean isExternalMap) {
+        for (var beEntry : beMap.entrySet()) {
             if (beEntry != null && beEntry.getValue() instanceof BlockEntityDogBowl bowl) {
                 var x = beEntry.getKey().getX();
                 var y = beEntry.getKey().getY();
                 var z = beEntry.getKey().getZ();
                 if (bowl.getFoodAmount() > 0 &&
-                        this.pet.distanceToSqr(x, y, z) < 50 &&
-                        //item should be closer than teleportation range
-                        this.owner != null && this.owner.distanceToSqr(x, y, z) < SophisticatedWolf.DISTANCE_TO_TELEPORT_TO_OWNER_SQR) {
-                    this.dogBowl = bowl;
+                        //bowl should be closer than teleportation range
+                        this.owner != null &&
+                        this.owner.distanceToSqr(x, y, z) < SophisticatedWolf.DISTANCE_TO_TELEPORT_TO_OWNER_SQR) {
+                    if (isExternalMap) {
+                        BOWLS.put(bowl.getBlockPos(), bowl);
+                    }
+                    this.feedObject = bowl;
                     return true;
                 }
             }
@@ -78,47 +107,5 @@ public class FeedFromBowlGoal extends Goal {
         return false;
     }
 
-    @Override
-    public boolean canContinueToUse() {
-        return this.timeToStopGoal <= 100 &&
-                this.pet.getHealth() < SWConfiguration.WOLVES_HEALTH_TAMED.get() &&
-                this.dogBowl != null && !this.dogBowl.isRemoved() && this.dogBowl.getFoodAmount() > 0 &&
-                //item should be closer than teleportation range
-                this.owner.distanceToSqr(
-                        this.dogBowl.getBlockPos().getX(),
-                        this.dogBowl.getBlockPos().getY(),
-                        this.dogBowl.getBlockPos().getZ()) < SophisticatedWolf.DISTANCE_TO_TELEPORT_TO_OWNER_SQR;
-    }
-
-    @Override
-    public void tick() {
-        if (this.dogBowl != null) {
-            timeToStopGoal++;
-            this.pet.getNavigation().moveTo(
-                    this.pet.getNavigation().createPath(this.dogBowl.getBlockPos(), 0),
-                    1);
-            var x = this.dogBowl.getBlockPos().getX();
-            var y = this.dogBowl.getBlockPos().getY();
-            var z = this.dogBowl.getBlockPos().getZ();
-            if (this.pet.distanceToSqr(x, y, z) <= 1.3F) {
-                this.pet.getLookControl().setLookAt(x, y, z, 0.25F, 0.25F);
-                this.pet.heal(1);
-
-                this.dogBowl.addFood(-1);
-                if (this.dogBowl.getFoodAmount() <= 0 ||
-                        this.pet.getHealth() >= SWConfiguration.WOLVES_HEALTH_TAMED.get()) {
-                    this.dogBowl = null;
-                    this.pet.getNavigation().stop();
-                }
-            }
-        }
-    }
-
-    @Override
-    public void stop() {
-        this.dogBowl = null;
-        this.timeToStopGoal = 0;
-        this.pet.getNavigation().stop();
-    }
 
 }
