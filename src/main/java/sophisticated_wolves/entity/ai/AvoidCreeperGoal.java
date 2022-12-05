7e52797af7da0b5cb0bdba46919e9e23b4d12715
NightKosh <nightkosh@gmail.com>
2022-12-06 02:42:42 +0300

    Flee AI, close #3

--- src/main/java/sophisticated_wolves/entity/ai/AvoidCreeperGoal.java
+++ src/main/java/sophisticated_wolves/entity/ai/AvoidCreeperGoal.java
@@ -1,13 +1,13 @@
 package sophisticated_wolves.entity.ai;
 
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.ai.util.DefaultRandomPos;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.level.pathfinder.Path;
-import net.minecraft.world.phys.Vec3;
+import sophisticated_wolves.entity.SophisticatedWolf;
 
 import java.util.function.Predicate;
 
@@ -19,32 +19,31 @@ import java.util.function.Predicate;
  */
 public class AvoidCreeperGoal extends Goal {
 
-    private final Predicate<Entity> canBeSeenSelector;
+    private static final int MIN_DIST_TO_CHARGED_CREEPER = 36;
+    private static final int SPRINT_DISTANCE = 49;
+
+    protected final Predicate<Entity> canBeSeenSelector;
     private final PathNavigation pathNavigation;
 
-    private final PathfinderMob mob;
-    private final double regSpeed;
-    private final double sprintSpeed;
-    private final int distance;
-    private final int minDistToCharged;
-    private final int minDist;
-    private final int minDist2;
-    private Creeper creeper;
+    protected final SophisticatedWolf wolf;
+    protected final double regSpeed;
+    protected final double sprintSpeed;
+    protected final int distance;
+    protected final int minDist;
+    protected final int minDist2;
+    protected Mob monster;
     private Path pathEntity;
 
-    public AvoidCreeperGoal(PathfinderMob mob, int distance, int minDistToExplode, int minDist, double regSpeed, double sprintSpeed) {
-        this.mob = mob;
+    public AvoidCreeperGoal(SophisticatedWolf wolf, int distance, int minDist, double regSpeed, double sprintSpeed) {
+        this.wolf = wolf;
         this.distance = distance;
-        this.minDistToCharged = minDistToExplode * minDistToExplode;
         this.minDist = minDist;
         this.minDist2 = minDist * minDist;
         this.regSpeed = regSpeed;
         this.sprintSpeed = sprintSpeed;
-        this.pathNavigation = mob.getNavigation();
+        this.pathNavigation = wolf.getNavigation();
 
-        this.canBeSeenSelector = prEntity ->
-                prEntity.isAlive() &&
-                        mob.hasLineOfSight(prEntity);
+        this.canBeSeenSelector = entity -> entity.isAlive() && wolf.hasLineOfSight(entity);
     }
 
     /**
@@ -52,34 +51,36 @@ public class AvoidCreeperGoal extends Goal {
      */
     @Override
     public boolean canUse() {
-        var creepers = this.mob.getLevel().getEntitiesOfClass(
+        var creepers = this.wolf.getLevel().getEntitiesOfClass(
                 Creeper.class,
-                this.mob.getBoundingBox()
+                this.wolf.getBoundingBox()
                         .inflate(this.distance, 3, this.distance),
                 this.canBeSeenSelector);
         for (var creeper : creepers) {
-            this.creeper = creeper;
 
-            if (this.creeper.isIgnited()) {
-                if (this.mob.distanceTo(this.creeper) < this.minDistToCharged) {
-                    return moveAway(16, 7);
+            if (creeper.isIgnited()) {
+                if (this.wolf.distanceTo(this.monster) < MIN_DIST_TO_CHARGED_CREEPER && moveAway(creeper, 16, 7)) {
+                    this.monster = creeper;
+                    return true;
                 }
-            } else if (this.mob.distanceTo(this.creeper) < this.minDist2) {
-                return moveAway(this.minDist, this.minDist);
+            } else if (this.wolf.distanceTo(this.monster) < this.minDist2 && moveAway(creeper)) {
+                this.monster = creeper;
+                return true;
             }
         }
         return false;
     }
 
-    protected boolean moveAway(int x, int y) {
-        Vec3 vec3 = DefaultRandomPos.getPosAway(this.mob, x, y, this.creeper.position());
+    protected boolean moveAway(Mob mob) {
+        return moveAway(mob, this.minDist, this.minDist);
+    }
 
-        if (vec3 != null) {
-            if (this.creeper.distanceToSqr(vec3.x, vec3.y, vec3.z) > this.creeper.distanceToSqr(this.mob)) {
-                this.pathEntity = this.pathNavigation.createPath(vec3.x, vec3.y, vec3.z, 0);
+    private boolean moveAway(Mob mob, int x, int y) {
+        var vec3 = DefaultRandomPos.getPosAway(this.wolf, x, y, mob.position());
 
-                return this.pathEntity != null;
-            }
+        if (vec3 != null && mob.distanceToSqr(vec3.x, vec3.y, vec3.z) > mob.distanceToSqr(this.wolf)) {
+            this.pathEntity = this.pathNavigation.createPath(vec3.x, vec3.y, vec3.z, 0);
+            return this.pathEntity != null;
         }
         return false;
     }
@@ -89,7 +90,7 @@ public class AvoidCreeperGoal extends Goal {
      */
     @Override
     public boolean canContinueToUse() {
-        return this.creeper.isAlive() && !this.pathNavigation.isDone();
+        return this.monster.isAlive() && !this.pathNavigation.isDone();
     }
 
     /**
@@ -97,7 +98,7 @@ public class AvoidCreeperGoal extends Goal {
      */
     @Override
     public void start() {
-        this.pathNavigation.moveTo(this.pathEntity, regSpeed);
+        this.pathNavigation.moveTo(this.pathEntity, this.regSpeed);
     }
 
     /**
@@ -105,7 +106,7 @@ public class AvoidCreeperGoal extends Goal {
      */
     @Override
     public void stop() {
-        this.creeper = null;
+        this.monster = null;
     }
 
     /**
@@ -113,7 +114,7 @@ public class AvoidCreeperGoal extends Goal {
      */
     @Override
     public void tick() {
-        this.mob.getNavigation().setSpeedModifier(this.mob.distanceTo(this.creeper) < 49 ?
+        this.wolf.getNavigation().setSpeedModifier(this.wolf.distanceTo(this.monster) < SPRINT_DISTANCE ?
                 this.sprintSpeed :
                 this.regSpeed);
     }
