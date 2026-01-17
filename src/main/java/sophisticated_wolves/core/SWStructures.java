02ac421a194af1948ed1a98343d7a1d190351fc4
NightKosh <nightkosh@gmail.com>
2026-01-17 01:13:37 +0100

    Fix

--- src/main/java/sophisticated_wolves/core/SWStructures.java
+++ src/main/java/sophisticated_wolves/core/SWStructures.java
@@ -15,7 +15,8 @@ import sophisticated_wolves.api.ModInfo;
 
 import java.util.ArrayList;
 
-import static net.minecraft.resources.Identifier.fromNamespaceAndPath;
+import static net.minecraft.resources.Identifier.withDefaultNamespace;
+import static sophisticated_wolves.SophisticatedWolvesMod.LOGGER;
 
 /**
  * Sophisticated Wolves
@@ -27,7 +28,7 @@ import static net.minecraft.resources.Identifier.fromNamespaceAndPath;
 public class SWStructures {
 
     private static final ResourceKey<StructureProcessorList> EMPTY_PROCESSOR_LIST_KEY =
-            ResourceKey.create(Registries.PROCESSOR_LIST, fromNamespaceAndPath("minecraft", "empty"));
+            ResourceKey.create(Registries.PROCESSOR_LIST, withDefaultNamespace("empty"));
 
     private static void addBuildingToPool(
             Registry<StructureTemplatePool> templatePoolRegistry,
@@ -35,37 +36,57 @@ public class SWStructures {
             Identifier poolRL,
             String nbtPieceRL,
             int weight) {
-        var pool = templatePoolRegistry.get(poolRL);
-        //TODO
-//        if (pool != null) {
-//            var emptyProcessorList = processorListRegistry.getHolderOrThrow(EMPTY_PROCESSOR_LIST_KEY);
-//            var piece = SinglePoolElement.legacy(nbtPieceRL, emptyProcessorList)
-//                    .apply(StructureTemplatePool.Projection.RIGID);
-//
-//            for (int i = 0; i < weight; i++) {
-//                pool.templates.add(piece);
-//            }
-//
-//            var listOfPieceEntries = new ArrayList<>(pool.rawTemplates);
-//            listOfPieceEntries.add(new Pair<>(piece, weight));
-//            pool.rawTemplates = listOfPieceEntries;
-//        }
+        var poolHolder = templatePoolRegistry.get(poolRL);
+        if (poolHolder.isPresent()) {
+            var pool = poolHolder.get().value();
+            var emptyProcessorList = processorListRegistry.get(EMPTY_PROCESSOR_LIST_KEY.identifier());
+            if (emptyProcessorList.isPresent()) {
+                var piece = SinglePoolElement.legacy(nbtPieceRL, emptyProcessorList.get())
+                        .apply(StructureTemplatePool.Projection.RIGID);
+
+                for (int i = 0; i < weight; i++) {
+                    pool.templates.add(piece);
+                }
+
+                var listOfPieceEntries = new ArrayList<>(pool.rawTemplates);
+                listOfPieceEntries.add(new Pair<>(piece, weight));
+                pool.rawTemplates = listOfPieceEntries;
+            }
+        }
     }
-//TODO
-//    @SubscribeEvent
-//    public static void addNewVillageBuilding(final ServerAboutToStartEvent event) {
-//        Registry<StructureTemplatePool> templatePoolRegistry =
-//                event.getServer().registryAccess().registryOrThrow(Registries.TEMPLATE_POOL);
-//        Registry<StructureProcessorList> processorListRegistry =
-//                event.getServer().registryAccess().registryOrThrow(Registries.PROCESSOR_LIST);
+
+    @SubscribeEvent
+    public static void addNewVillageBuilding(final ServerAboutToStartEvent event) {
+        if (SWConfiguration.DEBUG_MODE.get()) {
+            LOGGER.info("ServerAboutToStartEvent event triggered");
+        }
+//        var templatePoolRegistry = event.getServer()
+//                .registryAccess()
+//                .lookupOrThrow(Registries.TEMPLATE_POOL);
+//
+//        var processorListRegistry = event.getServer()
+//                .registryAccess()
+//                .lookupOrThrow(Registries.PROCESSOR_LIST);
 //
 //        addBuildingToPool(templatePoolRegistry, processorListRegistry,
-//                fromNamespaceAndPath("minecraft", "village/plains/houses"),
-//                "sophisticated_wolves:village/plains_kennels", 1);
+//                withDefaultNamespace("village/plains/houses"),
+//                "sophisticated_wolves:village/plains/dog_house", 100);
+
+//        addBuildingToPool(templatePoolRegistry, processorListRegistry,
+//                withDefaultNamespace("village/savanna/houses"),
+//                "sophisticated_wolves:village/savanna_kennels", 10);
 //
 //        addBuildingToPool(templatePoolRegistry, processorListRegistry,
-//                fromNamespaceAndPath("minecraft", "village/taiga/houses"),
-//                "sophisticated_wolves:village/taiga_kennels", 1);
-//    }
+//                withDefaultNamespace("village/desert/houses"),
+//                "sophisticated_wolves:village/desert_kennels", 10);
+//
+//        addBuildingToPool(templatePoolRegistry, processorListRegistry,
+//                withDefaultNamespace("village/taiga/houses"),
+//                "sophisticated_wolves:village/taiga_kennels", 10);
+//
+//        addBuildingToPool(templatePoolRegistry, processorListRegistry,
+//                withDefaultNamespace("village/snowy/houses"),
+//                "sophisticated_wolves:village/snowy_kennels", 10);
+    }
 
 }
